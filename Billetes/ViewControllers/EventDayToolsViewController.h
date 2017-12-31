2f7751b767a9df5586527643caf2bb40fc6095f7
ssaoirse <shawsaoirse@gmail.com>
2017-12-31 16:00:12 +0530

    SS - Added empty view controllers for required screens.

---
+++ Billetes/ViewControllers/EventDayToolsViewController.h
@@ -0,0 +1,13 @@
+//
+//  EventDayToolsViewController.h
+//  Billetes
+//
+//  Created by Saoirse on 12/31/17.
+//  Copyright © 2017 Boletos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EventDayToolsViewController : UIViewController
+
+@end
---
+++ Billetes/ViewControllers/EventDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  EventDetailViewController.h
+//  Billetes
+//
+//  Created by Saoirse on 12/31/17.
+//  Copyright © 2017 Boletos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EventDetailViewController : UIViewController
+
+@end

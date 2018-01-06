33460a3b6b6adb3be19a2c2d5317317aeea3b06f
ssaoirse <shawsaoirse@gmail.com>
2018-01-06 20:06:19 +0530

    SS - Added transport layer setting to allow accessing http urls.

---
+++ Billetes/Utilities/URLConstants.swift
@@ -0,0 +1,21 @@
+//
+//  URLConstants.swift
+//  Billetes
+//
+//  Created by Saoirse on 1/6/18.
+//  Copyright © 2018 Gaia Inc. All rights reserved.
+//
+
+import Foundation
+
+struct URLConstants {
+    
+    // MARK: -
+    // MARK: BaseURL -
+    static let kServerBaseURL                   = "http://dekhodemo.in"
+    
+    // MARK: -
+    // MARK: ServicePaths -
+    static let kLoginServicePath                = "/boletoexpress-api/public/login"
+    static let kLogoutServicePath               = "/boletoexpress-api/public/logout"
+}
